bb09a58d8701064774d1b2b07f074ccc1714d265
Jhon Bellido <jhonbellidovargas@gmail.com>
2023-01-16 11:21:28 -0500

    feat: activity 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'actividad-9';
+  data: Array<any> = [];
+  newData: any;
+  nombre: string = '';
+  apellido: string = '';
+  carrera: string = '';
+  id: number = 0;
+
+  addData() {
+    this.newData = {
+      id: this.id++,
+      nombre: this.nombre,
+      apellido: this.apellido,
+      carrera: this.carrera,
+    };
+    this.data.push(this.newData);
+    this.nombre = '';
+    this.apellido = '';
+    this.carrera = '';
+    console.log(this.data);
+  }
+
+  deleteData(id: number) {
+    this.data = this.data.filter((item) => item.id !== id);
+  }
 }
